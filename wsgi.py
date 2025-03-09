3ee12f469fb4c498f3b2f408bcceaf90041984d9
Your Name <user@example.com>
2025-03-09 13:22:52 +0100

    Ändere wsgi.py um simple_app anstatt app zu verwenden

--- wsgi.py
+++ wsgi.py
@@ -1,9 +1,8 @@
 import os
-from app import create_app
+from simple_app import app
 
-# Erstelle die Anwendungsinstanz mit der Standardkonfiguration oder
-# der Konfiguration aus der Umgebungsvariable
-app = create_app(os.getenv('FLASK_CONFIG') or 'default')
+# Hier ist keine create_app-Funktion nötig, da simple_app.py die App-Instanz direkt definiert
+# Wir müssen die app nur importieren
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=int(os.getenv('PORT', 5000))) 
